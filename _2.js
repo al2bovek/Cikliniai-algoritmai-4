563c40a8514d0cd2ecd09abdf982495ef3c01e44
D2JP <d2jp@TECHIN.INTERNAL>
2025-11-19 13:22:46 +0200

    commit _2.js

--- _2.js
+++ _2.js
@@ -1,6 +1,14 @@
  "use strict";
 //  2  Degalai.
+const kiekDienuTruksKelione = (t, n) => {
+    let d = 1;
+  while (t > 0) {
+  d % 2 !== 0 ? t -=2 * n : t -= n;
+  d++;
+  }
+  return `Keliauti bus galima ${d - 1} dienų/(as)/(ą)`
+}
+const bakoTalpa = prompt("bako talpa");
+const kuroSanaudas = prompt("kuro sanaudas")
 
-
-
-// document.getElementById("loop").innerHTML = ;
+document.getElementById("loop").innerHTML = kiekDienuTruksKelione(bakoTalpa, kuroSanaudas);
