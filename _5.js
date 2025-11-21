f2a7dae1aee3cc561e1847c87a9695151e3964da
al2bovek <aleksandr.dubovek@gmail.com>
2025-11-21 14:39:24 +0200

    commit _5.js

--- _5.js
+++ _5.js
@@ -1,31 +1,47 @@
 "use strict";
 // 5.  Kurjeris
-
-let suma = prompt("Įveskite sumą: ");
+const result = {
+    "Kiek Ivykdė Užsakymų": null,
+    "Vidutinę Sumą Per Dieną Išvežtu Prekių": null,
+    "Kiek Iš Viso Prekių Išvežiojo": null,
+}
 const parengtiPervežimųStatistiką = () => {
     const sumos = [];
-    let k;
     let kiekIvykdėUžsakymų;
-    while (sumos.at(-1) != 0) {
-        suma = prompt("Įveskite sumą: ");
+    while (true) {
+        const suma = prompt("Įveskite sumą: ");
+        if (suma == 0) {
+            break;
+        }
         sumos.push(suma);
     }
-     (kiekIvykdėUžsakymų = (arr) => {
-            k = 0;
-            arr.forEach(element => {
-                if (+element > 100) {
-                    k++;
-                    console.log(k);
-                    
-                }
-                    console.log(k);
-
-            });
-                    console.log(k);
-
-            return k;
-        })(sumos);
-return `1. ${k} `;
+    (kiekIvykdėUžsakymų = (arr) => {
+        let k = 0;
+        arr.forEach(element => {
+            if (+element > 100) {
+                k++;
+            }
+        });
+        kiekIvykdėUžsakymų = k;
+        return kiekIvykdėUžsakymų;
+    })(sumos);
+    const vidutinęSumą = (arr) => {
+        let sum = 0;
+        let forReturn;
+        if (arr.length === 0) {
+          forReturn = arr.length;  
+          return forReturn;
+        }
+        for (const element of arr) {
+            sum += +element;
+        }
+        forReturn = (sum / sumos.length).toFixed(2);
+        return forReturn;
+    }
+    result["Kiek Ivykdė Užsakymų"] = kiekIvykdėUžsakymų;
+    result["Vidutinę Sumą Per Dieną Išvežtu Prekių"] = vidutinęSumą(sumos);
+    result["Kiek Iš Viso Prekių Išvežiojo"] = sumos.length;
+    return result;
 }
-
-document.getElementById("loop").innerHTML = parengtiPervežimųStatistiką(suma);
+parengtiPervežimųStatistiką();
+document.getElementById("loop").innerHTML = `1. ${result["Kiek Ivykdė Užsakymų"]} <br> 2. ${result["Vidutinę Sumą Per Dieną Išvežtu Prekių"]} <br> 3. ${result["Kiek Iš Viso Prekių Išvežiojo"]}`;
