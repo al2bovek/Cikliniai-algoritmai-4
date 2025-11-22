e83bf570d45bb0720bb624035a64a300608090cf
al2bovek <aleksandr.dubovek@gmail.com>
2025-11-22 23:18:19 +0200

    commit _8.js

--- _8.js
+++ _8.js
@@ -1,6 +1,21 @@
- "use strict";
-
-
-
-
-// document.getElementById("loop").innerHTML = ;
+"use strict";
+// 8. Atvirukai. 
+const kiekRūšiųAtvirukųUžtektųVisiemsLinoDraugams = () => {
+    const m = +prompt(`Keliems draugams Linas nori 
+dovanoti atvirukus?`);
+    const n = +prompt(`Kiek rūšių atvirukų yra 
+parduotuvėje?`);
+    const k = [];
+    let x = 0;
+    while (k.length < n) {
+        k.push(+prompt(`Kiek yra rūšies atvirukų?`));
+    }
+    for (const el of k) {
+        if (el >= m) {
+            x++;
+        }
+    }
+    return x;
+}
+document.getElementById("loop").innerHTML = `${kiekRūšiųAtvirukųUžtektųVisiemsLinoDraugams()} rūšių atvirukų užtektų visiems Lino 
+draugams`;
