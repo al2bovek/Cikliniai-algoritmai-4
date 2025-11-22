11810c861d621b4c1f7af1c57c755e5b770771a7
al2bovek <aleksandr.dubovek@gmail.com>
2025-11-22 16:16:39 +0200

    commit _7.js

--- _7.js
+++ _7.js
@@ -1,6 +1,26 @@
- "use strict";
-
-
-
-
-// document.getElementById("loop").innerHTML = ;
+"use strict";
+// 7. Karnavalas.
+const result = {
+    ejo: null,
+    "Karnavalo Dalyviai": null,
+}
+const rengiamasKarnavalas = () => {
+    let ejo = 0;
+    const karnavaloDalyviai = [];
+    while (true) {
+        const karnavaloDalyvis = +prompt("Įveskite kiek balų gavo mokinys: nuo 1 iki 10 imtinai");
+        if (karnavaloDalyvis == 0) break;
+        if (karnavaloDalyvis < 0 || karnavaloDalyvis > 10) {
+            alert("Neteisingos duomenis");
+        }
+        ejo++;
+        if (karnavaloDalyvis >= 5 && karnavaloDalyvis <= 10) {
+            karnavaloDalyviai.push(karnavaloDalyvis);
+        }
+    }
+    result.ejo = ejo;
+    result["Karnavalo Dalyviai"] = karnavaloDalyviai.length;
+    return result;
+}
+rengiamasKarnavalas();
+document.getElementById("loop").innerHTML = `I karnavalą ėjo ${result.ejo} mokiniai, pateko ${result["Karnavalo Dalyviai"]}.`;
